556c5f2b4c056d25bb4d2f9062314a6f9eb3dcb8
Hanshen Wang <hanshenwang@outlook.com>
2021-05-20 13:22:21 -0400

    Add missing calfw-org package installation
    Fixes "Cannot open load file: No such file or directory, calfw-org"
    
    Credit Barry Ridge

--- packages.el
+++ packages.el
@@ -13,7 +13,8 @@
 
 (defconst google-calendar-packages
   '(org-gcal
-    calfw))
+    calfw
+    calfw-org))
 
 (defun google-calendar/init-org-gcal ()
   "Initializes org-gcal and adds keybindings for it's exposed functions"
@@ -72,13 +73,16 @@ other-frame                 Use `switch-to-buffer-other-frame' to display calend
     (define-key cfw:calendar-mode-map "N" 'cfw:navi-next-month-command)
     (define-key cfw:calendar-mode-map "P" 'cfw:navi-previous-month-command)
     (define-key cfw:calendar-mode-map "c" 'cfw:org-capture)
-    (define-key cfw:calendar-mode-map "v" 'cfw:org-open-agenda-day))
+    (define-key cfw:calendar-mode-map "v" 'cfw:org-open-agenda-day)))
+
+(defun google-calendar/init-calfw-org ()
+  "Initialize calfw-org"
 
   (use-package calfw-org
     :init
     (spacemacs/set-leader-keys
-      "agc" 'google-calendar/calfw-view)
-    (spacemacs/declare-prefix "agc" "open-org-calendar")
+      "aGc" 'google-calendar/calfw-view)
+    (spacemacs/declare-prefix "aGc" "open-org-calendar")
 
     :config
     (define-key cfw:org-schedule-map "q" 'google-calendar/calfw-restore-windows)
